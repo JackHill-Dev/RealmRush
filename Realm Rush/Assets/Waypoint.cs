1257d7ea1eaf58565dcfb5a4e6316c043b02567f
Jack Parkin <b7022282@my.shu.ac.uk>
2018-09-03 16:34:46 +0100

    Instance Variables and constants

--- Realm Rush/Assets/Waypoint.cs
+++ Realm Rush/Assets/Waypoint.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    Vector2Int gridPos;
 
-	// Use this for initialization
-	void Start ()
+    const int gridSize = 10;
+
+    // Use this for initialization
+    void Start ()
     {
 		
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    public int GetGridSize()
     {
-		
-	}
+        return gridSize;
+    }
+
+    public Vector2 GetGridPos()
+    {
+        return new Vector2Int(
+            Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
+            Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+        );
+    }
+
 }
